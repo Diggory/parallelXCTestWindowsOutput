e1b2d43c09fcd3174f8c0d0f9061e2ec168ed0c2
Diggory Laycock <nospam@diggory.net>
2026-05-08 08:47:49 +0100

    print test function names

--- Tests/parallelXCTestWindowsOutputTests/parallelXCTestWindowsOutputTests.swift
+++ Tests/parallelXCTestWindowsOutputTests/parallelXCTestWindowsOutputTests.swift
@@ -7,25 +7,30 @@ final class XCTestTests: XCTestCase {
 
     /// Should pass
     func test0_Instant() throws {
+        print(#function)
         XCTAssertEqual(1, 1)
     }
 
     func test1_Sleep3Seconds() throws {
+        print(#function)
         Thread.sleep(forTimeInterval: 3)
         XCTAssertEqual(1, 1)
     }
 
     func test2_Sleep2Seconds() throws {
+        print(#function)
         Thread.sleep(forTimeInterval: 2)
         XCTAssertEqual(1, 1)
     }
 
     func test3_Sleep1Second() throws {
+        print(#function)
         Thread.sleep(forTimeInterval: 1)
         XCTAssertEqual(1, 1)
     }
 
     func test4_Instant() throws {
+        print(#function)
         XCTAssertEqual(1, 1)
     }
 
